8b9173131a641e3ee045f1a001eee370e4e4da3c
ifeelblue99 <barancakirbey13@gmail.com>
2022-08-24 17:41:05 +0300

    bux fixed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * as colorPackages from "./colors/color-packs";
-export * as solidRGBColors from "./colors/solid-rgb-colors";
+export * from "./colors/color-packs";
+export * from "./colors/solid-rgb-colors";
 export * as Color from "./Color";
 export type { I_Color } from "./utils/colorTypes";
 export type { I_ColorPack } from "./utils/colorPack";
+export { buildColor } from "./utils/buildColor";
